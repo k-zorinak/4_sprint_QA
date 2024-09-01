f93bea9fd72fc97a2fa60d6890d5aa482b5f554a
Kamilla Rakhmatulina <kamilla.rakhmatulina@gmail.com>
2024-09-01 22:30:57 +0700

    Fix reviews

--- src/main/java/praktikum/HomePage.java
+++ src/main/java/praktikum/HomePage.java
@@ -10,79 +10,23 @@ import java.time.Duration;
 public class HomePage {
 
     public static final String URL = "https://qa-scooter.praktikum-services.ru/";
-    private WebDriver driver;
+    private final WebDriver driver;
 
     public By faqButtons = By.xpath("//div[contains(@class,'Home_FAQ')]");
-    private By closeCookie = By.xpath(".//button[text()='да все привыкли']");
-    private By faqHowMuch = By.xpath("//*[@id='accordion__heading-0']");
-    private By faqSeveralScooters = By.xpath("//*[@id='accordion__heading-1']");
-    private By faqRentalTime = By.xpath("//*[@id='accordion__heading-2']");
-    private By faqOrderToday = By.xpath("//*[@id='accordion__heading-3']");
-    private By faqExtendOrderOrEarlier = By.xpath("//*[@id='accordion__heading-4']");
-    private By faqBringChargers = By.xpath("//*[@id='accordion__heading-5']");
-    private By faqCancelOrder = By.xpath("//*[@id='accordion__heading-6']");
-    private By faqOutsideMoscowRingRoad = By.xpath("//*[@id='accordion__heading-7']");
-    public By faqHowMuchText = By.id("accordion__panel-0");
-    public By faqSeveralScootersText = By.id("accordion__panel-1");
-    public By faqRentalTimeText = By.id("accordion__panel-2");
-    public By faqOrderTodayText = By.id("accordion__panel-3");
-    public By faqExtendOrderOrEarlierText = By.id("accordion__panel-4");
-    public By faqBringChargersText = By.id("accordion__panel-5");
-    public By faqCancelOrderText = By.id("accordion__panel-6");
-    public By faqOutsideMoscowRingRoadText = By.id("accordion__panel-7");
+    private final By closeCookie = By.xpath(".//button[text()='да все привыкли']");
+
 
     public HomePage (WebDriver driver){
         this.driver = driver;
     }
 
-    public String openFaqHowMuch(){
-        driver.findElement(faqHowMuch).click();
+    public String openFaq(String numberOfQuestion){
+        driver.findElement(By.xpath("//*[@id='accordion__heading-" + numberOfQuestion + "']")).click();
         new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(faqHowMuchText));
-        return driver.findElement(faqHowMuchText).getText();
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-" + numberOfQuestion)));
+        return driver.findElement(By.id("accordion__panel-" + numberOfQuestion)).getText();
     }
-    public String openFaqSeveralScooters(){
-        driver.findElement(faqSeveralScooters).click();
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(faqSeveralScootersText));
-        return driver.findElement(faqSeveralScootersText).getText();
-    }
-    public String openFaqRentalTime(){
-        driver.findElement(faqRentalTime).click();
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(faqRentalTimeText));
-        return driver.findElement(faqRentalTimeText).getText();
-    }
-    public String openFaqOrderToday(){
-        driver.findElement(faqOrderToday).click();
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(faqOrderTodayText));
-        return driver.findElement(faqOrderTodayText).getText();
-    }
-    public String openFaqExtendOrderOrEarlier(){
-        driver.findElement(faqExtendOrderOrEarlier).click();
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(faqExtendOrderOrEarlierText));
-        return driver.findElement(faqExtendOrderOrEarlierText).getText();
-    }
-    public String openFaqBringChargers(){
-        driver.findElement(faqBringChargers).click();
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(faqBringChargersText));
-        return driver.findElement(faqBringChargersText).getText();
-    }
-    public String openFaqCancelOrder(){
-        driver.findElement(faqCancelOrder).click();
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(faqCancelOrderText));
-        return driver.findElement(faqCancelOrderText).getText();
-    }
-    public String openFaqOutsideMoscowRingRoad(){
-        driver.findElement(faqOutsideMoscowRingRoad).click();
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(faqOutsideMoscowRingRoadText));
-        return driver.findElement(faqOutsideMoscowRingRoadText).getText();
-    }
+
     public void clickCloseCookie(){
         driver.findElement(closeCookie).click();
     }
